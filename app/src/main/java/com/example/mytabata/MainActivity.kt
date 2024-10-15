ae7b4b003153f572fde828871458783a45fd2ce2
alumno <calucadoom@gmail.com>
2024-10-15 13:18:30 +0100

    Funciona

--- app/src/main/java/com/example/mytabata/MainActivity.kt
+++ app/src/main/java/com/example/mytabata/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mytabata
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,7 +12,12 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.mytabata.ui.theme.MyTabataTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,16 +38,50 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Counter(modifier: Modifier = Modifier) {
-    Column {
+    var theCounter by remember { mutableStateOf("90") }
+
+
+    Column(
+        modifier = modifier.padding(50.dp)
+    ) {
         Text(
-            text = "00",
-            modifier = modifier
+            text = theCounter
         )
-        Button(onClick = {}) {
+
+        Button(onClick = {
+            object : CountDownTimer(90000, 1000) {
+
+                override fun onTick(millisUntilFinished: Long) {
+                    theCounter = (millisUntilFinished / 1000).toString()
+                }
+
+                override fun onFinish() {
+                    theCounter = "Finish"
+                }
+            }.start()
+        }) {
             Text(
                 text = "pulsar"
             )
         }
     }
 }
+
+
+/*
+@Composable
+fun Countdown() {
+    object : CountDownTimer(30000, 1000) {
+
+        override fun onTick(millisUntilFinished: Long) {
+            mTextField.setText("seconds remaining: " + millisUntilFinished / 1000)
+        }
+
+        override fun onFinish() {
+            mTextField.setText("done!")
+        }
+    }.start()
+}
+
+ */
 
